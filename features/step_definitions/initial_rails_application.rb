f5e68a953fb5b0a45ec7abfa4b0585665469b5bc
Voarex <coltergellis@icloud.com>
2022-08-28 22:14:29 -0700

    feature[view]: add rows to product description
    set product description form to 10 rows
    
    #4: add number of rows to product description

---
+++ features/step_definitions/initial_rails_application.rb
@@ -0,0 +1,11 @@
+Given("there is a Ruby on Rails application called {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('you run the rails application') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("Puma should show the Rails application name as {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
