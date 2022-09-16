4b9eb71013d22b7009a77c732b85db43005a1e2d
Voarex <coltergellis@icloud.com>
2022-09-16 16:58:08 -0700

    feat[cart]: A Smarter Cart
    Adds a price column to an existing product table, with a default value
    Migrates existing data into the new table format
    Provides a flash notice of an error that was detected
    Uses the logger to log events
    Removes a parameter from the permitted list
    Deletes a record
    Adjusts the way a table is rendered, using Tailwind CSS classes
    
    #11: Feature: A Smarter Cart

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -54,11 +54,11 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "can't delete product in cart" do
-    assert_difference("Prouct.count", 0) do
+    assert_difference("Product.count", 0) do
       delete product_url(products(:two))
     end
 
-    assert_redirected_to product_url
+    assert_redirected_to products_url
   end
   
   test "should destroy product" do
